d4680321458c4c1f480170aedb8624034b82b63e
Rahul <prahul1590@gmail.com>
2024-05-02 23:43:08 -0400

    Added class for Network Manager Called api on home screen
    #TODO
    Image downloading

---
+++ Appetizers/APError.swift
@@ -0,0 +1,16 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Rahul Patel on 2024-05-02.
+//
+
+import Foundation
+
+
+enum APError:Error{
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
--- Appetizers/Tabbar/AppetizersListView.swift
+++ Appetizers/Tabbar/AppetizersListView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct AppetizersListView: View {
+    @State private var appetizers = [Appetizer]()
+    
     var body: some View {
         NavigationView{
-            List(MockData.appetizers){ appetizer in
+            List(appetizers){ appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .listStyle(PlainListStyle())
             .navigationTitle("🍟 Appetizers")
+        }
+        .onAppear(perform: {
+            getAppetizers()
+        })
+    }
+    
+    func getAppetizers(){
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result{
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 }
