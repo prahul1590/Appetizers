2c11166bac15dd84c706f89bebc7347fae302b7a
Rahul <prahul1590@gmail.com>
2024-05-08 13:40:07 -0400

    Add Badge on Order Tab

--- Appetizers/Screens/AppetizerListView/AppetizersListView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizersListView.swift
@@ -16,6 +16,7 @@ struct AppetizersListView: View {
             NavigationView{
                 List(viewModel.appetizers){ appetizer in
                     AppetizerListCell(appetizer: appetizer)
+//                        .listRowSeparator(.hidden)
                         .onTapGesture {
                             viewModel.selectedAppetizer = appetizer
                             viewModel.isShowingDetail = true
--- Appetizers/Screens/AppetizerTabView.swift
+++ Appetizers/Screens/AppetizerTabView.swift
@@ -8,23 +8,24 @@
 import SwiftUI
 
 struct AppetizerTabView: View {
+    
+    @EnvironmentObject var order:Order
+
     var body: some View {
         TabView{
-           AppetizersListView()
+            AppetizersListView()
                 .tabItem {
-                    Image(systemName: "house")
-                    Text("Home")
+                    Label("Home", systemImage: "house")
                 }
             AccountView()
-                 .tabItem {
-                     Image(systemName: "person")
-                     Text("Account")
-                 }
+                .tabItem {
+                    Label("Account", systemImage: "person")
+                }
             OrderView()
-                 .tabItem {
-                     Image(systemName: "bag")
-                     Text("Order")
-                 }
+                .tabItem {
+                    Label("Order", systemImage: "bag")
+                }
+                .badge(order.items.count)
         }
     }
 }
